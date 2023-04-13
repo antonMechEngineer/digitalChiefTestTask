b087bb79499210229d678229ef9cbfff1fd4b8ae
anetr <a.netrusov@mail.ru>
2023-04-13 20:32:51 +0300

    wrote README.md

--- src/main/java/company/CompanyApplication.java
+++ src/main/java/company/CompanyApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompanyApplication {
 
-	// TODO: 12.04.2023 как в dto передавать связки объектов??
-	// TODO: 12.04.2023 возможны проблемы с парсингом RQ
-	// TODO: 12.04.2023 на github заливать какую-то старую базу
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
